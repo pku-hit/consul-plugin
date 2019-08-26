69a4ed6a281fd2e3c5abfa3c6d47430138f8e1ee
huangguochao <917929050@qq.com>
2019-08-26 15:21:49 +0800

    <feat> 添加consul插件

---
+++ consul-plugin.go
@@ -0,0 +1,23 @@
+package consul_plugin
+
+import (
+	"github.com/micro/go-micro/registry"
+	"github.com/pku-hit/consul"
+)
+
+var Registry = struct {
+	Type string
+	Host string
+}{}
+
+var Reg registry.Registry
+
+func init() {
+	configor.Load(&Registry, "config/registry.yml")
+
+	Reg = consul.NewRegistry(func(op *registry.Options) {
+		op.Addrs = []string{
+			Registry.Host,
+		}
+	})
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/pku-hit/consul-plugin
+
+go 1.12
+
+require (
+	github.com/micro/go-micro v1.9.1
+	github.com/pku-hit/consul v1.1.0
+)
