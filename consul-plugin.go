75e3011bce5d6631d53c884f0e99d72a4f5744c9
huangguochao <917929050@qq.com>
2019-08-26 18:06:52 +0800

    <feat> 更换读取配置文件的方式

--- consul-plugin.go
+++ consul-plugin.go
@@ -1,9 +1,12 @@
 package consul_plugin
 
 import (
+	"fmt"
 	"github.com/jinzhu/configor"
 	"github.com/micro/go-micro/registry"
 	"github.com/pku-hit/consul"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 )
 
 var Registry = struct {
@@ -14,7 +17,11 @@ var Registry = struct {
 var Reg registry.Registry
 
 func init() {
-	configor.Load(&Registry, "config/registry.yml")
+	config, err := ioutil.ReadFile("config/registry.yml")
+	if err != nil {
+		fmt.Print(err)
+	}
+	yaml.Unmarshal(config, &Registry)
 
 	Reg = consul.NewRegistry(func(op *registry.Options) {
 		op.Addrs = []string{
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/jinzhu/configor v1.1.1
 	github.com/micro/go-micro v1.9.1
 	github.com/pku-hit/consul v1.2.0
+	gopkg.in/yaml.v2 v2.2.2
 )
